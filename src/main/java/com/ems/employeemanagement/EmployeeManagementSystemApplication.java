55983070495cb9dd5ea05b317e8bbe5772ff6292
suvigya001 <suvigya001@gmail.com>
2025-09-19 23:14:44 +0530

    package name refactored

--- src/main/java/com/ems/employeemanagement/EmployeeManagementSystemApplication.java
+++ src/main/java/com/ems/employeemanagement/EmployeeManagementSystemApplication.java
@@ -1,4 +1,4 @@
-package com.ems.employee_management_system;
+package com.ems.employeemanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/ems/employeemanagement/EmployeeManagementSystemApplicationTests.java
+++ src/test/java/com/ems/employeemanagement/EmployeeManagementSystemApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ems.employee_management_system;
+package com.ems.employeemanagement;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
